5bc6ad535571224aa065c393e1ba0ed9ce2e020c
Marius Darila <marius.darila@gmail.com>
2015-11-27 18:24:15 +0200

    added link to github

--- package.js
+++ package.js
@@ -4,9 +4,9 @@ Package.describe({
   name: 'akasha:shelljs',
   version: version,
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'shelljs package wrapped for meteor',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/AkashaProject/meteor-shelljs',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
